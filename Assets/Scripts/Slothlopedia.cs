572aa2cc14aed6ec6b40f8368ae68b146ed02e1e
Michael Stephens <mstephens.dev@gmail.com>
2014-08-30 22:51:00 -0400

    Started on slothlopedia, plus asset import
    Started on slothlopedia, plus asset import

---
+++ Assets/Scripts/Slothlopedia.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Slothlopedia : MonoBehaviour 
+{
+	#region Variables
+	
+		#region Public
+	
+		// The list of slothlopedia entry prefabs
+		public GameObject [] entryPrefabs;
+	
+		#endregion
+	
+	#endregion
+	
+	
+	#region Button Response
+	
+	//
+	//
+	public void SectionButtonPressed (int buttonNum)
+	{
+		print (buttonNum);
+	}
+	
+	#endregion
+	
+	
+	#region Initialization
+	
+	// Used for initialization
+	// Called automatically at beginning
+	void Start ()
+	{
+		// Assign the initial private/script/reference variables
+		AssignVariables ();
+	}
+	
+	
+	// Assigns the initial private/script/reference variables
+	// Called from Start ()
+	private void AssignVariables ()
+	{
+		
+	}
+	
+	#endregion
+}
